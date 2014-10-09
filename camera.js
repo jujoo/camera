11c0699b7ec5ed871d05c173fa17094068204ed3
jujoo <wnfks93@gmail.com>
2014-10-10 01:13:38 +0900

    camera 영상 재생

--- camera.js
+++ camera.js
@@ -1,9 +1,38 @@
-$(function(){
+
+
+
+// $(document).ready(function(){
+// 	$('#video')[0].play();
+// 	$('#video')[131000].stop(function(){
+// 		$(this).fadeOut(1000);
+// 	});
+// });
+$(document).ready(function(){
+ 	$('#video')[0].play();
+ 	$("#video").bind('ended', function(){
+		console.log("video ended");
+    	$("#video").fadeOut(500,function(){
+      	$("#video").remove();
+    	})
+    $("#masking").fadeIn(500);
+    $(function(){
 	$( '#example' ).photobooth().on( "image", function( event, dataUrl ){
 		$( "#gallery" ).show().html( '<img src="' + dataUrl + '" >');
+		});
+		setTimeout(function(){
+			$('.trigger').click();
+		}, 5000);
 	});
 
-	setTimeout(function(){
-		$('.trigger').click();
-	}, 15000);
+	})
 });
+
+
+// $(function(){
+// 	$( '#example' ).photobooth().on( "image", function( event, dataUrl ){
+// 		$( "#gallery" ).show().html( '<img src="' + dataUrl + '" >');
+// 	});
+// 	setTimeout(function(){
+// 		$('.trigger').click();
+// 	}, 136000);
+// });
